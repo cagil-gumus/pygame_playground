28f60463d561db25297ceecbbe52dedff7955ebd
guemues <cagil.guemues@desy.de>
2024-07-18 14:30:59 +0200

    switching to sprite style

--- first_game.py
+++ first_game.py
@@ -19,19 +19,20 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+WHITE = (255, 255, 255)
+
 
 # Put the object in the middle of the screen
 init_obj_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-object1 = object(init_position=init_obj_pos,
-                 init_velocity=[0.0, 0.0],
-                 weight=1.0,
-                 gravity=GRAVITY,
-                 radius = RADIUS,
-                 screen_height=SCREEN_HEIGHT,
-                 screen_width=SCREEN_WIDTH,
-                 air_resistance=AIR_RESISTANCE,
-                 dt_speed_up_factor=DT_SPEED_UP_FACTOR,
-                 collision_loss=COLLISION_LOSS)
+
+
+object_list = []
+
+
+# Sprites
+all_sprites = pygame.sprite.Group()
+
+object_to_add = 0
 
 while running:
     # poll for events
@@ -40,19 +41,32 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("white")
-    object1.update_dt(dt)
-
-    pygame.draw.circle(screen, "red", object1.position, RADIUS)
 
     keys = pygame.key.get_pressed()
-    object1.go_up = keys[pygame.K_w]
-    object1.go_down = keys[pygame.K_s]
-    object1.go_left = keys[pygame.K_a]
-    object1.go_right = keys[pygame.K_d]
+    if keys[pygame.K_0]:
+        object_list.append(object(init_position=init_obj_pos,
+                    init_velocity=[0.0, 0.0],
+                    weight=1.0,
+                    gravity=GRAVITY,
+                    radius = RADIUS,
+                    screen_height=SCREEN_HEIGHT,
+                    screen_width=SCREEN_WIDTH,
+                    air_resistance=AIR_RESISTANCE,
+                    dt_speed_up_factor=DT_SPEED_UP_FACTOR,
+                    collision_loss=COLLISION_LOSS))
+        all_sprites.add(object_list[object_to_add])
+        object_to_add += 1
 
-    # flip() the display to put your work on screen
+   # Update all sprites
+    all_sprites.update(dt)
+
+    # Clear the screen
+    screen.fill(WHITE)
+
+    # Draw all sprites
+    all_sprites.draw(screen)
+
+    # Flip the display
     pygame.display.flip()
 
     # limits FPS to 60
--- object.py
+++ object.py
@@ -1,108 +1,121 @@
 import numpy as np
-
-class object:
-  def __init__(self,
-               init_position, # left is pos, right is neg, up is neg, down is pos
-               init_velocity, # left is pos, right is neg, up is pos, down is neg
-               weight,
-               gravity, # Scalar
-               radius,
-               screen_width,
-               screen_height,
-               air_resistance,
-               dt_speed_up_factor,
-               collision_loss
-               ):
-
-    self.position = np.asarray(init_position) # m
-    self.velocity = np.asanyarray(init_velocity) # m/s^2
-    self.weight = weight # kg
-    self.gravity = gravity # in m/s^2 Scalar value
-    self.acceleration = np.zeros(2)
-    self.force = np.zeros(2)
-    self.go_down = False
-    self.go_up = False
-    self.go_left = False
-    self.go_right = False
-    self.dt = None
-    self.collision_x = False
-    self.collision_y = False
-    self.radius = radius
-    self.screen_width = screen_width
-    self.screen_height = screen_height
-    self.air_resistance =  air_resistance
-    self.dt_speed_up_factor = dt_speed_up_factor,
-    self.collision_loss = collision_loss
-
-  def update_dt(self, dt):
-    # Update important variables between delta-time
-    self.dt = dt * 10
-    self.handle_collision()
-    self.update_acceleration()
-    self.update_velocity()
-    self.update_position()
-    self.print_all_info()
-
-  # Things dt can effect:
-  def update_position(self):
-      self.position += self.velocity * self.dt
-
-  def update_velocity(self):
-    self.velocity += self.acceleration * self.dt
+import pygame
 
 
-  def update_acceleration(self):
+class object(pygame.sprite.Sprite):
+    def __init__(
+        self,
+        init_position,  # left is pos, right is neg, up is neg, down is pos
+        init_velocity,  # left is pos, right is neg, up is pos, down is neg
+        weight,
+        gravity,  # Scalar
+        radius,
+        screen_width,
+        screen_height,
+        air_resistance,
+        dt_speed_up_factor,
+        collision_loss,
+    ):
 
-    if self.collision_x:
-      self.acceleration[0] *= -self.collision_loss
-      self.velocity[0] *= -self.collision_loss
+        self.position = np.asarray(init_position)  # m
+        self.velocity = np.asanyarray(init_velocity)  # m/s^2
+        self.weight = weight  # kg
+        self.gravity = gravity  # in m/s^2 Scalar value
+        self.acceleration = np.zeros(2)
+        self.force = np.zeros(2)
+        self.go_down = False
+        self.go_up = False
+        self.go_left = False
+        self.go_right = False
+        self.dt = None
+        self.collision_x = False
+        self.collision_y = False
+        self.radius = radius
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.air_resistance = air_resistance
+        self.dt_speed_up_factor = (dt_speed_up_factor,)
+        self.collision_loss = collision_loss
 
-    if self.collision_y:
-      self.acceleration[1] *= -self.collision_loss
-      self.velocity[1] *= -self.collision_loss
+        # Sprite Related
+        super().__init__()
+        self.image = pygame.Surface((50, 50))
+        self.image.fill((0, 0, 255))  # Blue square
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.position[0], self.position[1])
 
-    if self.go_up:
-      self.acceleration[1] = -30
-    elif self.go_down:
-      self.acceleration[1] = 30 + self.gravity
-    else:
-      self.acceleration[1] = self.gravity
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        self.go_up = keys[pygame.K_w]
+        self.go_down = keys[pygame.K_s]
+        self.go_left = keys[pygame.K_a]
+        self.go_right = keys[pygame.K_d]
+        self.dt = dt * 10
+        self.handle_collision()
+        self.update_acceleration()
+        self.update_velocity()
+        self.update_position()
+        self.rect.center = (self.position[0], self.position[1])
+        # self.print_all_info()
 
-    if self.go_left and not self.go_right:
-      self.acceleration[0] = -30
-    elif not self.go_left and self.go_right:
-      self.acceleration[0] = 30
-    elif self.velocity[0] < 0:
-      self.acceleration[0] = self.air_resistance
-    elif self.velocity[0] > 0:
-      self.acceleration[0] = -1*self.air_resistance
-    else:
-       self.acceleration[0] = 0
+    # Things dt can effect:
+    def update_position(self):
+        self.position += self.velocity * self.dt
 
-  def handle_collision(self):
-      if (self.position[0] - self.radius < 0):
-        self.collision_x = True
-        self.position[0] = self.radius
-      elif (self.position[0] + self.radius) > self.screen_width:
-        self.collision_x = True
-        self.position[0] = self.screen_width - self.radius
-      else:
-          self.collision_x = False
+    def update_velocity(self):
+        self.velocity += self.acceleration * self.dt
 
-      if (self.position[1] - self.radius < 0):
-        self.collision_y = True
-        self.position[1] = self.radius
-      elif (self.position[1] + self.radius > self.screen_height):
-        self.collision_y = True
-        self.position[1] = self.screen_height - self.radius
-      else:
-          self.collision_y = False
+    def update_acceleration(self):
+        if self.collision_x:
+            self.acceleration[0] *= -self.collision_loss
+            self.velocity[0] *= -self.collision_loss
 
-  def print_all_info(self):
-    print("Force: {}".format(self.force))
-    print("Position: {}".format(self.position))
-    print("Velocity: {}".format(self.velocity))
-    print("Acceleration: {}".format(self.acceleration))
-    print("Dt: {}".format(self.dt))
-    print("Collision-X: {}".format(self.collision_x))
-    print("Collision-y: {}".format(self.collision_y))
+        if self.collision_y:
+            self.acceleration[1] *= -self.collision_loss
+            self.velocity[1] *= -self.collision_loss
+
+        if self.go_up:
+            self.acceleration[1] = -30
+        elif self.go_down:
+            self.acceleration[1] = 30 + self.gravity
+        else:
+            self.acceleration[1] = self.gravity
+
+        if self.go_left and not self.go_right:
+            self.acceleration[0] = -30
+        elif not self.go_left and self.go_right:
+            self.acceleration[0] = 30
+        elif self.velocity[0] < 0:
+            self.acceleration[0] = self.air_resistance
+        elif self.velocity[0] > 0:
+            self.acceleration[0] = -1 * self.air_resistance
+        else:
+            self.acceleration[0] = 0
+
+    def handle_collision(self):
+        if self.position[0] - self.radius < 0:
+            self.collision_x = True
+            self.position[0] = self.radius
+        elif (self.position[0] + self.radius) > self.screen_width:
+            self.collision_x = True
+            self.position[0] = self.screen_width - self.radius
+        else:
+            self.collision_x = False
+
+        if self.position[1] - self.radius < 0:
+            self.collision_y = True
+            self.position[1] = self.radius
+        elif self.position[1] + self.radius > self.screen_height:
+            self.collision_y = True
+            self.position[1] = self.screen_height - self.radius
+        else:
+            self.collision_y = False
+
+    def print_all_info(self):
+        print("Force: {}".format(self.force))
+        print("Position: {}".format(self.position))
+        print("Velocity: {}".format(self.velocity))
+        print("Acceleration: {}".format(self.acceleration))
+        print("Dt: {}".format(self.dt))
+        print("Collision-X: {}".format(self.collision_x))
+        print("Collision-y: {}".format(self.collision_y))
